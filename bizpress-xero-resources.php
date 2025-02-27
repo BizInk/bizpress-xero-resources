91edd737d6f1110400add48e05bca366c9fb0368
Jayden Major <jaydenmajor@gmail.com>
2025-02-27 10:57:47 +1000

    Fix for XML Sitemaps

--- bizpress-xero-resources.php
+++ bizpress-xero-resources.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://bizinkonline.com
  * Author: Bizink
  * Author URI: https://bizinkonline.com
- * Version: 1.3.4
+ * Version: 1.3.5
  * Text Domain: bizink-client
  * Domain Path: /languages
  */
@@ -21,9 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 $myUpdateChecker = PucFactory::buildUpdateChecker('https://github.com/BizInk/bizpress-xero-resources',__FILE__,'bizpress-xero-resources');
-// Set the branch that contains the stable release.
 $myUpdateChecker->setBranch('master');
-// Using a private repository, specify the access token 
 $myUpdateChecker->setAuthentication('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');
 
 if(is_plugin_active("bizpress-client/bizink-client.php")){
--- xero-resources.php
+++ xero-resources.php
@@ -124,10 +124,8 @@ function bizpress_xeroxml_request($wp){
 			}
 			header('Content-Type: text/xml; charset=UTF-8');
 			echo '<?xml version="1.0" encoding="UTF-8"?>';
-			$url = get_home_url();
-			$url = str_replace('https:','',$url);
-			$url = str_replace('http:','',$url);
-			echo '<?xml-stylesheet type="text/xsl" href="//'.$url.'/wp-content/plugins/wordpress-seo/css/main-sitemap.xsl"?>';
+			
+			echo '<?xml-stylesheet type="text/xsl" href="'. plugins_url('wordpress-seo/css/main-sitemap.xsl', dirname(__FILE__)) .'"?>';
 			echo '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 			
 			echo '<url>';
