b53db29092b5b39484777b2e2a53b280562fa5fd
Jayden Major <jayden@jaydenmajor.com>
2025-04-03 08:22:38 +1000

    Updater Fix

--- bizpress-xero-resources.php
+++ bizpress-xero-resources.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://bizinkonline.com
  * Author: Bizink
  * Author URI: https://bizinkonline.com
- * Version: 1.3.5
+ * Version: 1.3.6
  * Text Domain: bizink-client
  * Domain Path: /languages
  */
@@ -22,7 +22,7 @@ require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 $myUpdateChecker = PucFactory::buildUpdateChecker('https://github.com/BizInk/bizpress-xero-resources',__FILE__,'bizpress-xero-resources');
 $myUpdateChecker->setBranch('master');
-$myUpdateChecker->setAuthentication('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');
+$myUpdateChecker->setAuthentication('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');
 
 if(is_plugin_active("bizpress-client/bizink-client.php")){
 	require 'xero-resources.php';
